cc1c4e76258baa8f4936c2ae93a21242f9fb2934
Andres Galvan <andresdaniel.go2020@gmail.com>
2023-03-21 00:46:26 -0500

    Actualizacion funciones

--- problemaClase.cpp
+++ problemaClase.cpp
@@ -9,15 +9,12 @@ struct Nodo{
 void insertarCola(Nodo *&,Nodo *&,int);
 bool cola_vacia(Nodo *);
 void suprimirCola(Nodo *&,Nodo *&,int &);
-void bfs(Nodo *&,Nodo *&,int &);
-int main(){
-
+void bfs(Nodo *&,Nodo *&,int &,int &);
+int main()
+{
 	int opc;
 	int dato,datoBuscar;
-	
 	Nodo *frente = NULL;
-	
-	
 	Nodo *fin = NULL;
 
 	do{
@@ -36,24 +33,32 @@ int main(){
 			case 2: cout<<"\nMostrando los elementos de la cola: ";
 					while(frente != NULL){
 						suprimirCola(frente,fin,dato);
-						if(frente != NULL){
-							cout<<dato<<" , ";
-						}
-						else{
-							cout<<dato<<".";
-						}
+						// if(frente != NULL){
+						// 	cout<<dato<<" , ";
+						// }
+						// else{
+						// 	cout<<dato<<".";
+						// }
 					}
 					cout<<"\n";
 					system("pause");
 					break;
 			case 3: cout<<"\nBuscando elemento de la cola: ";
                     cout<<"\nIngrese el dato a buscar: ";cin>>datoBuscar;
-                    bfs(frente,fin,datoBuscar);
+                    while(frente != NULL){
+						bfs(frente,fin,dato,datoBuscar);
+						// if(frente != NULL){
+						// 	cout<<dato<<" , ";
+						// }
+						// else{
+						// 	cout<<dato<<".";
+						// }
+					}
 					break;
 			case 4: break;
 		}
 		system("cls");
-	}while(opc != 3);
+	}while(opc != 4 );
 
 	return 0;
 }
@@ -87,40 +92,49 @@ void suprimirCola(Nodo *&frente,Nodo *&fin,int &n){
 
 	if(frente == fin)
     {
+        cout<<"\nElemento: "<<frente->dato;
 		frente = NULL;
 		fin = NULL;
+        
 	}
 	else
     {
-		frente = frente->siguiente;
-        cout<<frente->dato<<" "<<endl;
+        cout<<"\nElemento: "<<frente->dato;
+		frente = frente->siguiente;   
 	}
 	delete aux;
 }
 
-void bfs(Nodo *&frente, Nodo *&fin, int &datoBuscar)
+void bfs(Nodo *&frente, Nodo *&fin, int &datoBuscar,int &dato)
 {
-    Nodo *actual = frente;
-    int encontrado = 0;
+    dato = frente->dato;
+	Nodo *aux = frente;
 
-    while (actual != NULL)
+	if(frente == fin && datoBuscar==frente->dato)
     {
-        if (actual->dato == datoBuscar)
-        {
-            encontrado = 1;
-            break;
-        }
-
-        actual = actual->siguiente;
-        cout<<actual->dato<<endl;
-    }
-
-    if (encontrado == 1)
+        cout<<"\nElemento: "<<frente->dato;
+		cout<<"Se encontro el elemento al final";
+        frente = NULL;
+		fin = NULL;
+        
+	}
+    else if(frente == fin)
     {
-        cout << "Se encontró el valor: " << datoBuscar << endl;
+        cout<<"\nElemento: "<<frente->dato;
+        frente = NULL;
+		fin = NULL;
     }
-    else
+    else if(frente->dato == datoBuscar )
     {
-        cout << "No se encontró el valor: " << datoBuscar << endl;
+        cout<<"\nElemento: "<<frente->dato;
+		cout<<"Se encontro el elemento ";
+        //El break
+        frente = NULL;
+		fin = NULL;
     }
+	else
+    {
+        cout<<"\nElemento: "<<frente->dato;
+		frente = frente->siguiente;   
+	}
 }
